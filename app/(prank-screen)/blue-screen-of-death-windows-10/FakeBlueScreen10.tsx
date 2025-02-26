ac33d40043092f63a3e747a3ca1d1bcb891e00a9
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-02-26 11:12:24 +0700

    hoapq - refactor - fix css fake blue screen

--- app/(prank-screen)/blue-screen-of-death-windows-10/FakeBlueScreen10.tsx
+++ app/(prank-screen)/blue-screen-of-death-windows-10/FakeBlueScreen10.tsx
@@ -16,29 +16,61 @@ export default function FakeBlueScreen10() {
 
   return (
     <div
-      className={`p-4 bg-[#0078D7] font-sans ${
+      className={`bg-[#0078D7] text-white h-full cursor-pointer ${
         isFullscreen
-          ? "fixed inset-0 z-50 flex items-center justify-center"
+          ? "fixed inset-0 z-50 flex flex-col items-center justify-center"
           : ""
       }`}
     >
-      <div className="">:(</div>
-      <div className="">
-        Your PC ran into a problem and needs to restart. We&apos;re just
-        collecting some error info, and then we&apos;ll restart for you.
-      </div>
-      <div className="">
-        <span className="">{progress}</span>% complete
-      </div>
-      <div className="">
-        <Image src={windowQRCode} alt="QR Code" width={100} height={100} />
-        <div className="">
-          <p>
-            For more information about this issue and possible fixes, visit
-            https://www.windows.com/stopcode
+      <div
+        className={`w-4/5 flex flex-col items-start gap-4  ${isFullscreen ? "justify-center " : " mx-auto pt-6  text-[.52rem]"}`}
+      >
+        <p className={isFullscreen ? "text-9xl" : "text-6xl"}>:(</p>
+        <div className={isFullscreen ? "w-1/2 text-wrap" : "w-full text-wrap"}>
+          <p
+            className={
+              isFullscreen
+                ? "text-4xl font-light leading-tight"
+                : "text-[.9rem] font-light"
+            }
+          >
+            Your PC ran into a problem and needs to restart. We&apos;re just
+            collecting some error info, and then we&apos;ll restart for you.
           </p>
-          <p>If you call a support person, give them this info:</p>
-          <p>Stop code: CRITICAL_PROCESS_DIED</p>
+
+          <div className={isFullscreen ? "my-8" : "my-4"}>
+            <p
+              className={`inline-block mr-2 ${isFullscreen ? "text-4xl" : "text-lg "}`}
+            >
+              {progress}%
+            </p>
+            <p
+              className={`inline-block font-light ${isFullscreen ? "text-3xl" : "text-[.9rem]"}`}
+            >
+              complete
+            </p>
+          </div>
+
+          <div className="flex gap-4 items-start">
+            <Image
+              src={windowQRCode}
+              alt="QR Code"
+              width={isFullscreen ? 120 : 60}
+              height={isFullscreen ? 120 : 60}
+            />
+            <div
+              className={`${isFullscreen ? "text-[.8rem]" : "text-[.4rem] opacity-80"} `}
+            >
+              <p>
+                For more information about this issue and possible fixes, visit
+                https://www.windows.com/stopcode
+              </p>
+              <p className={isFullscreen ? "mt-12 mb-4" : "mt-6 mb-2"}>
+                If you call a support person, give them this info:
+              </p>
+              <p>Stop code: CRITICAL_PROCESS_DIED</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
