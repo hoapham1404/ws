919f82e1cc9f9dfbdbcbfe3f7ff13437e2d52d9a
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-05 22:45:24 +0700

    refactor(color-options): Enhance color route navigation with active state and improved styling

--- components/BasicColorPage/ColorOptions.tsx
+++ components/BasicColorPage/ColorOptions.tsx
@@ -1,25 +1,37 @@
-import { getColorRoutes } from '@/constants/routes'
+import { getColorRoutes, RouteStore } from '@/constants/routes'
 import { useTranslations } from 'next-intl'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 
 export default function ColorOptions() {
   const colorRoutes = getColorRoutes()
   const t = useTranslations()
+  const pathname = usePathname()
+
 
   return (
-    <>
-      {colorRoutes.filter(route => route.isAxis === true && route.color != null && route.color != undefined).map((route) => (
-        <Link key={route.path} href={route.path} className="flex items-center gap-5 w-full md:w-80 px-4 py-2 text-left rounded hover:opacity-90 underline">
-          <div
-            className="w-12 md:w-14 h-8 rounded"
-            style={{
-              backgroundColor: route.color,
-              boxShadow: `0 0 16px 0 rgba(0, 0, 0, 0.3)`
-            }}
-          ></div>
-          <span>{t(`${route.path}.name`)}</span>
-        </Link>
-      ))}
-    </>
+    <div className="h-full flex flex-col justify-between gap-4">
+      {
+        colorRoutes.filter((route: RouteStore) => route.isAxis && route.color).map((route: RouteStore) => {
+          return (
+            <Link
+              key={route.path}
+              href={route.path}
+              className={`h-full px-4 py-2 flex items-center  gap-5 text-left rounded text-wrap
+                ${pathname === route.path ? 'bg-gray-200' : ''}
+              `}
+            >
+              <div className={"w-16 h-8 rounded"}
+                style={{
+                  backgroundColor: route.color,
+                  boxShadow: `0 0 16px 0 rgba(0, 0, 0, 0.3)`
+                }}
+              />
+              <span className={"underline "}>{t(`${route.path}.name`)}</span>
+            </Link>
+          )
+        })
+      }
+    </div>
   )
 }
