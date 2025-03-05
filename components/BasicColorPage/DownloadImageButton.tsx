76e8515557aae8a690aedc63c14e1c8bf7293641
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-05 19:01:20 +0700

    feat(i18n): Add download button translation for color screen

--- components/BasicColorPage/DownloadImageButton.tsx
+++ components/BasicColorPage/DownloadImageButton.tsx
@@ -1,14 +1,16 @@
 import { colorStore } from "@/store/colorStore";
 import downloadImage from "@/lib/downloadImage";
+import { useTranslations } from "next-intl";
 
 export default function DownloadImageButton() {
   const { currentResolution, currentColor } = colorStore((state) => state);
+  const t = useTranslations("settings.color");
   return (
     <button
       onClick={() => downloadImage(currentResolution, currentColor)}
       className="w-full py-2 px-4 border rounded bg-white hover:bg-gray-50 mt-4"
     >
-      Download
+      {t("0")}
     </button>
   );
 }
--- components/BasicColorPage/SettingsPanel.tsx
+++ components/BasicColorPage/SettingsPanel.tsx
@@ -1,24 +1,21 @@
 'use client'
-import { getRouteByPath } from "@/constants/routes";
-import { colorStore } from "@/store/colorStore"
 import { usePathname } from "next/navigation";
 import ResolutionOptions from "./ResolutionOptions";
 import Temperature from "./Temperature";
+import { useTranslations } from "next-intl";
+import DownloadImageButton from "@/components/BasicColorPage/DownloadImageButton";
 
 
 export default function SettingsPanel() {
   const currentPath = usePathname();
-  const currentRoute = getRouteByPath(currentPath);
-  const { setCurrentColor, setTemperature, setResolution } = colorStore();
+  const t = useTranslations(currentPath);
 
   return (
-    <>
-      <div className="flex flex-col items-center">
-        <h3 className="mb-4 ml-4 text-2xl ">{currentRoute?.name}</h3>
-        <ResolutionOptions />
-
-        <Temperature />
-      </div>
-    </>
+    <div className="flex flex-col items-center">
+      <h3 className="mb-4 ml-4 text-2xl ">{t("name")}</h3>
+      <ResolutionOptions />
+      <Temperature />
+      <DownloadImageButton />
+    </div>
   )
 }
