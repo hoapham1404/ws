f3ffaf4178e5d7f1b8710ec9213ab807b9fa857d
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-06 14:43:53 +0700

    feat(ui): Add fullscreen responsiveness to FakeChromeOS component

--- components/FakeChromeOS/FakeChromeOS.tsx
+++ components/FakeChromeOS/FakeChromeOS.tsx
@@ -1,39 +1,41 @@
-    import { FC } from 'react';
-    import ChromeOSLogo from "@/public/chrome-logo.png";
-    import Image from 'next/image';
+import { FC } from 'react';
+import ChromeOSLogo from "@/public/chrome-logo.png";
+import Image from 'next/image';
+import { useFullScreen } from '../(prank-screen)/hooks/useFullScreen';
 
-    const FakeChromeOS: FC = () => {
-    return (
-        <div className="w-full h-full bg-[#1f1f1f] flex-col items-center justify-center text-white">
-        <div className="flex flex-col items-start justify-center h-full p-8">
-            <div className='flex items-center justify-center'>
-                <div className="flex items-center justify-center">
-                <Image
-                    src={ChromeOSLogo}
-                    alt="Chrome OS"
-                    width={20}
-                    height={20}
-                />
-                </div>
-                <div className='ml-2'>chromeOS</div>
-            </div>
+const FakeChromeOS: FC = () => {
+  const { isFullscreen } = useFullScreen();
+  return (
+    <div className="h-full bg-[#1f1f1f] flex  items-center justify-center text-white">
+      <div className={`h-full ${isFullscreen ? 'w-2/3' : 'w-full'} flex flex-col items-start justify-center`}>
+        <div className='flex items-center justify-center'>
+          <div className="flex items-center justify-center">
+            <Image
+              src={ChromeOSLogo}
+              alt="Chrome OS"
+              width={isFullscreen ? 40 : 20}
+              height={isFullscreen ? 40 : 20}
+            />
+          </div>
+          <div className='ml-2 text-sm'>chromeOS</div>
+        </div>
 
-            <hr className='w-full my-2' />
-            
-            <div className="flex items-center gap-4">
-            <div className="w-14 h-14 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
-            <div className='flex flex-col gap-6'>
-                <h2 className="text-sm">Firmware updating</h2>
-                <p className="text-sm">
-                    Do not turn off or unplug your device while update
-                    <br />is in progress. This may take a few minutes.
-                </p>
-            </div>
-            </div>
         <hr className='w-full my-2' />
-        </div>
-        </div>
-    );
-    };
 
-    export default FakeChromeOS;
+        <div className={`flex items-center ${isFullscreen ? 'm-[20%] gap-10' : 'my-4 gap-4'}`}>
+          <div className={`${isFullscreen ? 'w-14' : 'w-14'} aspect-square border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin`} />
+          <div className={`flex flex-col gap-6 ${isFullscreen ? 'text-2xl' : 'text-sm'}`}>
+            <h2>Firmware updating</h2>
+            <p>
+              Do not turn off or unplug your device while update
+              <br />is in progress. This may take a few minutes.
+            </p>
+          </div>
+        </div>
+        <hr className='w-full my-2' />
+      </div>
+    </div >
+  );
+};
+
+export default FakeChromeOS;
