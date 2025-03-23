3975d9b0c215cd13ae7e56287e8a3ef6f97e053f
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-23 11:37:50 +0700

    fixUI

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,64 +1,75 @@
-import { cn } from "@/lib/utils"
-import { ChevronLeft, ChevronRight, Navigation, Quote } from "lucide-react"
-import { useFullScreen } from "../(prank-screen)/hooks/useFullScreen"
-import SidebarTab from "./SidebarTab"
+import { cn } from "@/lib/utils";
+import { ChevronLeft, ChevronRight, Navigation, Quote } from "lucide-react";
+import { useFullScreen } from "../(prank-screen)/hooks/useFullScreen";
+import SidebarTab from "./SidebarTab";
 import Image from "next/image";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
-import sidebarStore from "./useSidebar"
-import { Button } from "../ui/button"
-import { usePathname } from "next/navigation"
-import { useTranslations } from "next-intl"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import sidebarStore from "./useSidebar";
+import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 export default function Sidebar() {
-  const { isFullscreen } = useFullScreen()
+  const { isFullscreen } = useFullScreen();
   const pathName = usePathname();
   const t = useTranslations(pathName);
   const raw = useTranslations();
-  const { isPanelOpen, setIsPanelOpen, setCurrentTab } = sidebarStore()
+  const { isPanelOpen, setIsPanelOpen, setCurrentTab } = sidebarStore();
 
   return (
     <div className="">
-      {!isFullscreen &&
+      {!isFullscreen && (
         <div
           className={cn(
             "fixed top-1/2 left-4 transform -translate-y-1/2 transition-all duration-300 ease-in-out",
             "z-10",
-            "min-h-80  bg-white text-gray-900",
+            "bg-white text-gray-900",
             "rounded-xl shadow-xl border",
             isPanelOpen ? "min-w-80" : "min-w-12",
           )}
         >
           <div className="  h-full max-h-[750px] overflow-y-auto">
-            {isPanelOpen && (
-              <section className="p-4 flex flex-row justify-between items-center border-b">
-                <p className="font-bold">{t("name")}</p>
-              </section>
-            )}
-
-            {isPanelOpen && (
-              <section className="p-4">
-                <SidebarTab />
-              </section>
-            )}
+            {isPanelOpen && <SidebarTab />}
 
             {isPanelOpen && (
               <div className="p-2 text-sm text-center flex flex-col items-center justify-center gap-1">
                 <p className="">
-                  {
-                    Array.from(new Map([
+                  {Array.from(
+                    new Map([
                       ["/privacy-policy", "Privacy policy"],
                       ["/terms-and-conditions", "Terms & conditions"],
-                      ["/contact-us", "Contact us"]
-                    ]).entries()).map(([key, value]) => (
-                      <a key={key} href={key} onClick={(e) => { e.preventDefault(); window.location.href = key }} className="mx-1
+                      ["/contact-us", "Contact us"],
+                    ]).entries(),
+                  ).map(([key, value]) => (
+                    <a
+                      key={key}
+                      href={key}
+                      onClick={(e) => {
+                        e.preventDefault();
+                        window.location.href = key;
+                      }}
+                      className="mx-1
                     hover:underline transition-colors duration-300 ease-in-out
-                    ">{value}</a>
-                    ))
-                  }
+                    "
+                    >
+                      {value}
+                    </a>
+                  ))}
                 </p>
                 <div className="flex flex-row justify-center items-center gap-1">
                   <p className="">© 2025 WS, Made in US </p>
-                  <Image src="https://flagcdn.com/w320/us.png" alt="US Flag" className="inline-block" width={20} height={20} />
+                  <Image
+                    src="https://flagcdn.com/w320/us.png"
+                    alt="US Flag"
+                    className="inline-block"
+                    width={20}
+                    height={20}
+                  />
                 </div>
               </div>
             )}
@@ -73,9 +84,10 @@ export default function Sidebar() {
                         size="icon"
                         className="h-8 w-8"
                         onClick={() => {
-                          setIsPanelOpen(true)
-                          setCurrentTab(1)
-                        }}>
+                          setIsPanelOpen(true);
+                          setCurrentTab(1);
+                        }}
+                      >
                         <Navigation className="h-4 w-4" />
                       </Button>
                     </TooltipTrigger>
@@ -93,8 +105,8 @@ export default function Sidebar() {
                         size="icon"
                         className="h-8 w-8"
                         onClick={() => {
-                          setIsPanelOpen(true)
-                          setCurrentTab(2)
+                          setIsPanelOpen(true);
+                          setCurrentTab(2);
                         }}
                       >
                         <Quote className="h-4 w-4" />
@@ -112,13 +124,16 @@ export default function Sidebar() {
               onClick={() => setIsPanelOpen(!isPanelOpen)}
               className="absolute top-1/2 right-0 transform -translate-y-1/2 translate-x-1/2 w-8 h-8 flex items-center justify-center rounded-full bg-white shadow-2xl z-20 transition-all duration-300 ease-in-out "
             >
-              {isPanelOpen ? <ChevronLeft className="w-4 h-4" /> : <ChevronRight className="w-4 h-4" />}
+              {isPanelOpen ? (
+                <ChevronLeft className="w-4 h-4" />
+              ) : (
+                <ChevronRight className="w-4 h-4" />
+              )}
             </button>
-            <div>
-            </div>
-          </div >
+            <div></div>
+          </div>
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
--- components/Sidebar/SidebarTab.tsx
+++ components/Sidebar/SidebarTab.tsx
@@ -4,27 +4,39 @@ import NavigationSection from "./NavigationSection";
 import TipsSection from "./TipsSection";
 import sidebarStore from "./useSidebar";
 import { useTranslations } from "next-intl";
-
+import { usePathname } from "next/navigation";
 
 export default function SidebarTab(): JSX.Element {
   const { currentTab, setCurrentTab } = sidebarStore();
   const t = useTranslations("sidebar");
-  const tabs: { id: number, name: string; content: JSX.Element }[] = [
+  const pathName = usePathname();
+  const thead = useTranslations(pathName);
+  const tabs: { id: number; name: string; content: JSX.Element }[] = [
     {
       id: 1,
       name: t("links"),
-      content: <NavigationSection />
+      content: <NavigationSection />,
     },
     {
       id: 2,
       name: t("uses"),
-      content: <TipsSection />
-    }
+      content: <TipsSection />,
+    },
   ];
 
   return (
     <div>
-      <div className={cn("flex flex-row justify-between items-center", "border rounded-xl", "text-sm overflow-hidden")}>
+      <section className="p-4 flex flex-row justify-between items-center border-b">
+        <p className="font-bold">{thead("name")}</p>
+      </section>
+
+      <div
+        className={cn(
+          "flex flex-row justify-between items-center",
+          "border rounded-xl",
+          "text-sm overflow-hidden",
+        )}
+      >
         {tabs.map((tab) => (
           <button
             key={tab.id}
@@ -34,7 +46,9 @@ export default function SidebarTab(): JSX.Element {
               "p-2 cursor-pointer",
               "transition-all duration-300 ease-in-out",
               "border-b",
-              currentTab === tab.id ? "border-blue-500" : "border-transparent hover:border-gray-300"
+              currentTab === tab.id
+                ? "border-blue-500"
+                : "border-transparent hover:border-gray-300",
             )}
           >
             {tab.name}
@@ -42,15 +56,15 @@ export default function SidebarTab(): JSX.Element {
         ))}
       </div>
       <div className="mt-4">
-
-        {
-          tabs.map((tab) => (
-            <div key={tab.id} className={cn(currentTab === tab.id ? "block" : "hidden")}>
-              {tab.content}
-            </div>
-          ))
-        }
+        {tabs.map((tab) => (
+          <div
+            key={tab.id}
+            className={cn(currentTab === tab.id ? "block" : "hidden")}
+          >
+            {tab.content}
+          </div>
+        ))}
       </div>
-    </div >
+    </div>
   );
 }
