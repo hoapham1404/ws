e3340dd3a0e7cde4a2ded71e2019fa925beaa6ad
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-23 11:08:44 +0700

    hoapq - fix - fix LocalesDropdown UI

--- components/LocalesDropdown.tsx
+++ components/LocalesDropdown.tsx
@@ -12,13 +12,13 @@ export default function LocalesDropdown() {
      * Deutschland, English, France, Schweiz, Nederland, 한국어
      */
     const labels: Map<string, string> = new Map([
-      ['de', 'Deutschland'], // Germany
-      ['en', 'English'], // English
-      ['fr', 'France'], // France
-      ['nl', 'Nederland'], // Netherlands
-      ['cn', '中國'], // China
-      ['ko', '한국인'], // Korean
-    ])
+      ["de", "Deutschland"], // Germany
+      ["en", "English"], // English
+      ["fr", "France"], // France
+      ["nl", "Nederland"], // Netherlands
+      ["cn", "中國"], // China
+      ["ko", "한국인"], // Korean
+    ]);
 
     return labels.get(locale) || locale;
   };
@@ -33,23 +33,25 @@ export default function LocalesDropdown() {
           {getLanguageLabel(activeLocale || "")}
         </span>
         {isOpen && (
-          <div className="absolute top-full left-1/2 transform -translate-x-1/2 z-10 shadow-[0_8px_30px_rgba(0,0,0,.32)] w-32">
+          <div className="absolute top-full left-1/2 transform -translate-x-1/2 z-10 shadow-[0_8px_30px_rgba(0,0,0,.32)] w-32 bg-white">
             <div className="flex flex-col gap-2 text-left text-base">
-              {locales?.filter(locale => locale !== activeLocale).map((locale) => (
-                <Link
-                  href={asPath}
-                  locale={locale}
-                  key={locale}
-                  onClick={() => setIsOpen(false)}
-                  className="px-4 py-2 hover:bg-gray-300"
-                >
-                  {getLanguageLabel(locale)}
-                </Link>
-              ))}
+              {locales
+                ?.filter((locale) => locale !== activeLocale)
+                .map((locale) => (
+                  <Link
+                    href={asPath}
+                    locale={locale}
+                    key={locale}
+                    onClick={() => setIsOpen(false)}
+                    className="px-4 py-2 hover:bg-gray-300"
+                  >
+                    {getLanguageLabel(locale)}
+                  </Link>
+                ))}
             </div>
           </div>
         )}
       </button>
     </div>
-  )
+  );
 }
