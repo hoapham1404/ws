2decc8d261beec711724ac8333413d34ff9327d5
hoapham2k2 <20520995@gm.uit.edu.vn>
2025-03-26 17:18:33 +0700

    hoapq - fix - add circle color navigation routes

--- components/BasicColorPage/ColorOptions.tsx
+++ components/BasicColorPage/ColorOptions.tsx
@@ -1,37 +1,37 @@
-import { getColorRoutes, RouteStore } from '@/constants/routes'
-import { useTranslations } from 'next-intl'
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import { getColorRoutes, RouteStore } from "@/constants/routes";
+import { useTranslations } from "next-intl";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function ColorOptions() {
-  const colorRoutes = getColorRoutes()
-  const t = useTranslations()
-  const pathname = usePathname()
-
+  const colorRoutes = getColorRoutes();
+  const t = useTranslations();
+  const pathname = usePathname();
 
   return (
     <div className="h-full flex flex-col justify-between gap-4">
-      {
-        colorRoutes.filter((route: RouteStore) => route.isAxis && route.color).map((route: RouteStore) => {
+      {colorRoutes
+        .filter((route: RouteStore) => route.isAxis && route.color)
+        .map((route: RouteStore) => {
           return (
             <Link
               key={route.path}
               href={route.path}
               className={`h-full px-4 py-2 flex items-center  gap-5 text-left rounded text-wrap
-                ${pathname === route.path ? 'bg-gray-200' : ''}
+                ${pathname === route.path ? "bg-gray-200" : ""}
               `}
             >
-              <div className={"w-16 h-8 rounded"}
+              <div
+                className={"w-16 h-8 rounded"}
                 style={{
                   backgroundColor: route.color,
-                  boxShadow: `0 0 16px 0 rgba(0, 0, 0, 0.3)`
+                  boxShadow: `0 0 16px 0 rgba(0, 0, 0, 0.3)`,
                 }}
               />
               <span className={"underline "}>{t(`${route.path}.name`)}</span>
             </Link>
-          )
-        })
-      }
+          );
+        })}
     </div>
-  )
+  );
 }
--- components/BasicColorPage/NavigationBottom.tsx
+++ components/BasicColorPage/NavigationBottom.tsx
@@ -3,7 +3,7 @@ import { useTranslations } from "next-intl";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Image, { StaticImageData } from "next/image";
-
+import { cn } from "@/lib/utils";
 export default function NavigationBottom({ routes }: { routes: RouteStore[] }) {
   const currentPath = usePathname();
   const t = useTranslations();
@@ -14,22 +14,40 @@ export default function NavigationBottom({ routes }: { routes: RouteStore[] }) {
         <Link
           key={route.path}
           href={route.path}
-          className={`w-full flex flex-col items-center p-6 ${route.path === currentPath ? 'bg-gray-300' : ''}`}
+          className={`w-full flex flex-col items-center p-6 ${route.path === currentPath ? "bg-gray-300" : ""}`}
         >
-          <div className="shadow-md">
-            {
-              route.color ?
-                <div className="w-32 aspect-video rounded-md shadow-md hover:opacity-90 transition-opacity" style={{ backgroundColor: route.color }}
-                />
-                :
-                <Image src={route.thumbnail as StaticImageData} alt={route.path} className="w-32 aspect-video object-cover rounded-lg" />
-            }
-          </div>
+          {route.color ? (
+            <div className="">
+              <div
+                className={cn(
+                  "w-28 aspect-square hover:opacity-90 transition-opacity",
+                  "rounded-full overflow-hidden",
+                )}
+                style={{
+                  backgroundColor: route.color,
+                  boxShadow: " 0 8px 30px rgba(0, 0, 0, .32)",
+                }}
+              />
+            </div>
+          ) : (
+            <Image
+              src={route.thumbnail as StaticImageData}
+              alt={route.path}
+              className={cn(
+                "w-32 aspect-video object-cover rounded-lg",
+                "overflow-hidden",
+              )}
+              style={{
+                boxShadow: " 0 8px 30px rgba(0, 0, 0, .32)",
+              }}
+            />
+          )}
 
-          <span className="mt-2 text-sm md:text-base underline text-center">{t(`${route.path}.name`)}</span>
+          <span className="mt-2 text-sm md:text-base underline text-center">
+            {t(`${route.path}.name`)}
+          </span>
         </Link>
-      ))
-      }
-    </div >
-  )
+      ))}
+    </div>
+  );
 }
